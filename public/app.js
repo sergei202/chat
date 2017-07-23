695d8fbad176021afc8f35f6917f43e081140fc4
Sergei <sergei@sleekbook>
2017-07-23 15:54:52 -0500

    Add server-side logic.  It is now a fully working chat app.

--- public/app.js
+++ public/app.js
@@ -1,21 +1,40 @@
 angular.module('app', []);
 
-angular.module('app').controller('ChatCtrl', function($scope,$http) {
-	$scope.messages = [
-		{name:'Dummy', text:'First message', date:new Date()},
-		{name:'Dummy', text:'Hi',			 date:new Date()}
-	];
+angular.module('app').controller('ChatCtrl', function($scope,$http,$interval) {
 
-	$scope.name = 'Sergei';
+	// Get list of messages from server backend route
+	function refresh() {
+		$http.get('/messages').then(function(res) {
+			console.log('/messages: ', res.data);
+			$scope.messages = res.data;
+		});
+	}
+	refresh();
+
+	// Call refresh() every second to get the latest messages
+	$interval(refresh, 1000);
+
+	// Initialize name and newMessage
+	$scope.name = '';
 	$scope.newMessage = {text:''};
 
 
 	$scope.send = function() {
 		console.log('send() newMessage=%o', $scope.newMessage);
+		// newMessage right now only contains the text,
+		// we need to add name and date
 		$scope.newMessage.name = $scope.name;
 		$scope.newMessage.date = new Date();
-		$scope.messages.push($scope.newMessage);
 
+		// POST newMessage to the backend
+		$http.post('/message', $scope.newMessage).then(function(res) {
+			console.log('/message: ', res.data);
+			// res.data contains the new message list.
+			// This prevents us from having to call refresh()
+			$scope.messages = res.data;
+		});
+
+		// Reset newMessage to clear the last text
 		$scope.newMessage = {text:''};
 	};
 });
--- server.js
+++ server.js
@@ -15,3 +15,30 @@ app.use('/', express.static('./public'));
 app.listen(3000, function(err) {
 	console.log('Listening at http://localhost:3000');
 });
+
+
+var messages = [
+	{name:'Server', text:'Node server started', date:new Date()},
+];
+
+// This block adds 100 messages to show that we really are getting the last 10
+// for(var i=0;i<100;i++) {
+// 	messages.push({name:'Repeat', text:'Message #' + i, date:new Date()});
+// }
+
+
+// Route to get all the messages
+app.get('/messages', function(req,res) {
+	// Show only the last 10 messages
+	res.json(messages.slice(-10));
+});
+
+
+// Route to add a new message
+app.post('/message', function(req,res) {
+	var message = req.body;
+	console.log('/message: ', message);
+	messages.push(message);
+	// Show only the last 10 messages
+	res.json(messages.slice(-10));
+});
